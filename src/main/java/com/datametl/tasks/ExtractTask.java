76328d7313e8e31040e74509c760c8e54568c202
andywtse <tsea@wit.edu>
2017-02-23 12:07:08 -0500

    Added ExtractTask

--- src/main/java/com/datametl/tasks/ExtractTask.java
+++ src/main/java/com/datametl/tasks/ExtractTask.java
@@ -1,7 +1,47 @@
 package com.datametl.tasks;
 
+import com.datametl.jobcontrol.JobState;
+import com.datametl.jobcontrol.SubJob;
+import org.json.JSONObject;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by TseAndy on 2/6/17.
  */
-public class Extract {
+public class ExtractTask implements Task{
+
+    private JobState returnCode;
+    private SubJob parent = null;
+    private Map<String,Collection<String>> table = new HashMap<String, Collection<String>>();
+
+    public void apply() {
+
+        JSONObject etlPacket = parent.getETLPacketFromParent();
+        String filePath = etlPacket.getJSONObject("source").getString("path");
+        String docToRead = etlPacket.getJSONObject("documents_to_read").toString();
+
+
+    }
+
+    public JobState getResult() {
+        return returnCode;
+    }
+
+    public void setParent(SubJob parent) {
+        this.parent = parent;
+    }
+
+    public SubJob getParent() {
+        return parent;
+    }
+
+    private void dataExtract(){
+
+
+
+        return;
+    }
 }
